73ec529621a92bfbe09f22b3512b029881ccb502
ms.moradi23 <ms.moradi23@yahoo.com>
2022-11-23 03:41:46 +0330

    correction paths and pages

--- src/route/route.js
+++ src/route/route.js
@@ -1,17 +1,28 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home/Home";
+import Menu from "../pages/Menu/Menu";
+import Layout from "../components/Layout/Layout";
 import SinglePageCity from "../pages/SinglePageCity/SinglePageCity";
 
 const router = createBrowserRouter([
-    
     {
-        path:'/',
-        element:<Home/>
-    },
-    {
-        path:"/singlePageCity/:cityID",
-        element:<SinglePageCity/>
-    }
+        path:"/",
+        element:<Layout/>,
+        children:[
+            {
+                index:true,
+                element:<Home/>,
+            },
+            {
+                path:"/menu",
+                element:<Menu/>
+            },
+            {
+                path:"/singlePageCity/:cityID",
+                element:<SinglePageCity/>
+            }
+        ]
+    } 
 ])
 
 export default router;
